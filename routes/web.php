d5b68da212a09a3071efbc49f166aa366a50d82a
Calixto Mantal <cyriesmadrid@gmail.com>
2024-09-30 17:51:11 +0800

    Done Initial Setup Dedicated dashboard views

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,33 @@ Route::get('/', function () {
 
 
 //Temporary commented
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified',
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard/patient', function () {
+        return view('dashboards.patient');
+    })->name('dashboard.patient');
+
+    Route::get('/dashboard/doctor', function () {
+        return view('dashboards.doctor');
+    })->name('dashboard.doctor');
+
+    Route::get('/dashboard/administrator', function () {
+        return view('dashboards.administrator');
+    })->name('dashboard.administrator');
+
+    Route::get('/dashboard/super-admin', function () {
+        return view('dashboards.superadmin');
+    })->name('dashboard.superadmin');
+});
+
