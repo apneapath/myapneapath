d0311b76f4c9523ed8e07ac30296d00d116b4360
Calixto Mantal <cyriesmadrid@gmail.com>
2024-09-30 15:57:39 +0800

    Done Storefront Initial Setup

--- resources/views/storefront/index.blade.php
+++ resources/views/storefront/index.blade.php
@@ -36,42 +36,11 @@
         ======================================================== -->
     </head>
     <body class="font-sans antialiased">
-        <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-
-            @if (Route::has('login'))
-                <nav class="-mx-3 flex flex-1 justify-end">
-                    @auth
-                        <a
-                            href="{{ url('/dashboard') }}"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-                        >
-                            Dashboard
-                        </a>
-                    @else
-                        <a
-                            href="{{ route('login') }}"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-                        >
-                            Log in
-                        </a>
-
-                        @if (Route::has('register'))
-                            <a
-                                href="{{ route('register') }}"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-                            >
-                                Register
-                            </a>
-                        @endif
-                    @endauth
-                </nav>
-            @endif
-        </header>
 
         <header id="header" class="header sticky-top">
 
             <div class="topbar d-flex align-items-center">
-              <div class="container d-flex justify-content-center justify-content-md-between">
+              <div class="container-fluid p-5 d-flex justify-content-center justify-content-md-between">
                 <div class="contact-info d-flex align-items-center">
                   <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
                   <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
@@ -87,7 +56,7 @@
         
             <div class="branding d-flex align-items-center">
         
-              <div class="container position-relative d-flex align-items-center justify-content-between">
+              <div class="container-fluid ps-5 pt-0 pe-5 pb-0 position-relative d-flex align-items-center justify-content-between">
               
                 <div>
                     <a href="/">
@@ -96,33 +65,45 @@
                 </div>
         
                 <nav id="navmenu" class="navmenu">
-                  <ul>
-                    <li><a href="#hero" class="active">Home<br></a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#departments">Departments</a></li>
-                    <li><a href="#doctors">Doctors</a></li>
-                    <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                      <ul>
-                        <li><a href="#">Dropdown 1</a></li>
-                        <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                          <ul>
-                            <li><a href="#">Deep Dropdown 1</a></li>
-                            <li><a href="#">Deep Dropdown 2</a></li>
-                            <li><a href="#">Deep Dropdown 3</a></li>
-                            <li><a href="#">Deep Dropdown 4</a></li>
-                            <li><a href="#">Deep Dropdown 5</a></li>
-                          </ul>
+                    <ul>
+                        <li><a href="#hero" class="active">Home<br></a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#departments">Departments</a></li>
+                        <li><a href="#doctors">Doctors</a></li>
+                        <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                            <ul>
+                                <li><a href="#">Dropdown 1</a></li>
+                                <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                    <ul>
+                                        <li><a href="#">Deep Dropdown 1</a></li>
+                                        <li><a href="#">Deep Dropdown 2</a></li>
+                                        <li><a href="#">Deep Dropdown 3</a></li>
+                                        <li><a href="#">Deep Dropdown 4</a></li>
+                                        <li><a href="#">Deep Dropdown 5</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Dropdown 2</a></li>
+                                <li><a href="#">Dropdown 3</a></li>
+                                <li><a href="#">Dropdown 4</a></li>
+                            </ul>
                         </li>
-                        <li><a href="#">Dropdown 2</a></li>
-                        <li><a href="#">Dropdown 3</a></li>
-                        <li><a href="#">Dropdown 4</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="#contact">Contact</a></li>
-                  </ul>
-                  <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+                        <li><a href="#contact">Contact</a></li>
+                
+                        @if (Route::has('login'))
+                            @auth
+                                <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
+                            @else
+                                <li><a href="{{ route('login') }}">Log In</a></li>
+                                @if (Route::has('register'))
+                                    <li><a href="{{ route('register') }}">Register</a></li>
+                                @endif
+                            @endauth
+                        @endif
+                    </ul>
+                    <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
                 </nav>
+                
         
                 <a class="cta-btn d-none d-sm-block" href="#appointment">Make an Appointment</a>
         
