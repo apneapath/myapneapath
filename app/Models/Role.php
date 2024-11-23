89f1d53697dcb941fac33921642a5cde7b5b7af4
Calixto Mantal <cyriesmadrid@gmail.com>
2024-11-23 11:21:48 +0800

    Initial Done For Overall RBAC (Role Base Access Control) it's working expectedly from assigning role and permission to the user as well as updating their permission with the same role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,21 +5,6 @@ namespace App\Models;
 // use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Role as SpatieRole; // Import Spatie Role class
 
-// class Role extends Model
-// {
-//     protected $fillable = ['name'];
-
-//     public function users()
-//     {
-//         return $this->belongsToMany(User::class);
-//     }
-
-//     public function permissions()
-//     {
-//         return $this->belongsToMany(Permission::class);
-//     }
-// }
-
 class Role extends SpatieRole
 {
     // No need to add methods like 'users()' and 'permissions()' here,
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,102 @@
 <?php
 
+// namespace App\Models;
+
+// // use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Notifications\Notifiable;
+// use Laravel\Fortify\TwoFactorAuthenticatable;
+// use Laravel\Jetstream\HasProfilePhoto;
+// use Laravel\Sanctum\HasApiTokens;
+// use Spatie\Permission\Traits\HasRoles; // Add this import
+
+// class User extends Authenticatable
+// {
+//     use HasApiTokens;
+//     use HasFactory;
+//     use HasProfilePhoto;
+//     use Notifiable;
+//     use TwoFactorAuthenticatable;
+//     use HasRoles; // Add this line to enable roles and permissions functionality
+
+//     /**
+//      * The attributes that are mass assignable.
+//      *
+//      * @var array<int, string>
+//      */
+//     protected $fillable = [
+//         'name',
+//         'firstName',
+//         'lastName',
+//         'gender',
+//         'email',
+//         'address',
+//         'phoneNumber',
+//         'role',
+//         'status',
+//         'userName',
+//         'password',
+//     ];
+
+
+//     /**
+//      * The attributes that should be hidden for serialization.
+//      *
+//      * @var array<int, string>
+//      */
+//     protected $hidden = [
+//         'password',
+//         'remember_token',
+//         'two_factor_recovery_codes',
+//         'two_factor_secret',
+//     ];
+
+//     /**
+//      * The accessors to append to the model's array form.
+//      *
+//      * @var array<int, string>
+//      */
+//     protected $appends = [
+//         'profile_photo_url',
+//     ];
+
+//     /**
+//      * Get the attributes that should be cast.
+//      *
+//      * @return array<string, string>
+//      */
+//     protected function casts(): array
+//     {
+//         return [
+//             'email_verified_at' => 'datetime',
+//             'password' => 'hashed',
+//         ];
+//     }
+
+
+//     public function roles()
+//     {
+//         return $this->belongsToMany(Role::class);
+//     }
+
+//     public function hasRole($role)
+//     {
+//         return $this->roles()->where('name', $role)->exists();
+//     }
+
+//     public function hasPermission($permission)
+//     {
+//         return $this->roles()->whereHas('permissions', function ($query) use ($permission) {
+//             $query->where('name', $permission);
+//         })->exists();
+//     }
+// }
+
+
+
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -20,11 +114,6 @@ class User extends Authenticatable
     use TwoFactorAuthenticatable;
     use HasRoles; // Add this line to enable roles and permissions functionality
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'firstName',
@@ -39,12 +128,6 @@ class User extends Authenticatable
         'password',
     ];
 
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
@@ -52,20 +135,10 @@ class User extends Authenticatable
         'two_factor_secret',
     ];
 
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array<int, string>
-     */
     protected $appends = [
         'profile_photo_url',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -74,10 +147,14 @@ class User extends Authenticatable
         ];
     }
 
-
     public function roles()
     {
         return $this->belongsToMany(Role::class);
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
     }
 
     public function hasRole($role)
@@ -91,4 +168,16 @@ class User extends Authenticatable
             $query->where('name', $permission);
         })->exists();
     }
+
+    // New helper methods
+    public function hasPermissionTo($permission)
+    {
+        return $this->hasPermission($permission) || $this->permissions->contains('name', $permission);
+    }
+
+    public function isAdmin()
+    {
+        return $this->hasRole('Super Admin');
+    }
 }
+
